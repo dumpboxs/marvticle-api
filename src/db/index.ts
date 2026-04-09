2aa26f1eb5e1b7fe34a45761e989d9a3ca10ad2d
MrBoxs <rizkywanda200155@gmail.com>
2026-04-09 23:45:36 +0700

    feat(db): migrate from neon to docker postgres

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,12 +1,15 @@
-﻿import { neon } from '@neondatabase/serverless'
-import { drizzle } from 'drizzle-orm/neon-http'
+import { drizzle } from 'drizzle-orm/node-postgres'
+import { Pool } from 'pg'
 
 import { env } from '#/env'
 import * as schema from '#/db/schemas'
 
 export function createDb() {
-  const sql = neon(env.DATABASE_URL)
-  return drizzle(sql, { schema })
+  const pool = new Pool({
+    connectionString: env.DATABASE_URL,
+  })
+
+  return drizzle({ client: pool, schema })
 }
 
 export const db = createDb()
